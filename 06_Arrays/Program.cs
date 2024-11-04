58702b3a2e17e77471a005ffd4b7b418330f2655
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-11-04 17:26:41 +0300

    changes edited

--- 06_Arrays/Program.cs
+++ 06_Arrays/Program.cs
@@ -76,19 +76,98 @@ namespace _06_Arrays
             //}
             //********************************************
 
-            int[] myArray = { 47, 85, 95, 41, 25, 635, 897, 86, 789, 100, 1100, };
-            int maxNumber = myArray[0];
-            for (int i = 1; i < myArray.Length; i++)
-            {
-                if (myArray[i] > maxNumber)
-                {
-                    maxNumber = myArray[i];
-                }
-            }
-            Console.WriteLine(maxNumber );
+            //int[] myArray = { 47, 85, 95, 41, 25, 635, 897, 86, 789, 100, 1100, };
+            //int maxNumber = myArray[0];
+            //for (int i = 1; i < myArray.Length; i++)
+            //{
+            //    if (myArray[i] > maxNumber)
+            //    {
+            //        maxNumber = myArray[i];
+            //    }
+            //}
+            //Console.WriteLine(maxNumber );
+            //********************************************
+
+            //string[] persons = { "ali", "ahmet", "ayşe", "buse", "cem", "deniz" };
+            //Console.WriteLine(persons.Length);
+            //********************************************
+
+            //int[] numbers = { 45, 85, 52, 41, 86, 10, 22, 35 };
+            //Array.Sort(numbers);
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+            //********************************************
+
+            //int[] numbers = { 45, 85, 52, 41, 86, 10, 22, 35 };
+            //Array.Reverse(numbers);
+            //for(int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
 
             #endregion
 
+            #region Dizi Metotları
+
+            //string[] customers = { "ali", "buse", "ayşegül", "merve", "çınar", "kaya" };
+            //int index = Array.IndexOf(customers, "merve");
+            //Console.WriteLine(index);
+
+            //**********************************************
+
+            //int[] numbers = { 45, 85, 96, 63, 74, 10, 25, 22, 36 };
+            //Console.WriteLine("Dizinin En Küçük Elemanı:" + numbers.Min() + " Dizinin En Büyük Elemanı:" + numbers.Max());
+            //**********************************************
+            #endregion
+
+            #region Kullanıcıdan Değer Alma
+
+            //string[] cities = new string[5];
+            //for (int i = 0; i < cities.Length; i++)
+            //{
+            //    Console.Write($"Lütfen {i + 1}.Şehri Giriniz: ");
+            //    cities[i] = Console.ReadLine();
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("------------------------------");
+            //for (int i = 0; i < cities.Length; i++)
+            //{
+            //    Console.WriteLine(cities[i]);
+            //}
+            //********************************************************
+
+            //int[] numbers = { 10, 20, 30, 40, 50 };
+            //int sum = 0;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    sum += numbers[i];
+            //}
+            //Console.WriteLine(sum);
+            //********************************************************
+
+            int[] numbers = { 220, 21, 42, 33, 54, 55, 66, 897, 748, 39 };
+            Console.WriteLine("Çift Sayılar");
+            Console.WriteLine();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                    Console.WriteLine(numbers[i]);
+            }
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Tek Sayılar");
+            Console.WriteLine();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 1)
+                    Console.WriteLine(numbers[i]);
+
+            }
+
+
+            #endregion
             Console.Read();
 
         }
